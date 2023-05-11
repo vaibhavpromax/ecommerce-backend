01a9ca75c6e571aea89a5c356bc48c77877e8791
Vaibhav <singhvaibhav557hr@gmail.com>
2023-05-11 19:09:33 +0530

    change

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const logger = require("./utils/logger");
 const port = process.env.PORT || 8080;
 const { NODE_ENV } = process.env;
 
+server.listen(port, () =>
+  logger.info(
+    `[BACKEND-MICROSERVICE (http) LISTENING ON PORT:${port} ENV:${NODE_ENV}]`
+  )
+);
 sequelize
   .sync()
   .then(() => {
     logger.info("[CONNECTED TO DATABASE]");
-    server.listen(port, () =>
-      logger.info(
-        `[BACKEND-MICROSERVICE (http) LISTENING ON PORT:${port} ENV:${NODE_ENV}]`
-      )
-    );
   })
   .catch((err) => {
     logger.error("Failed to connect to db", err);
